052b4d167bc3b24d015af447052bcc8845419683
efrawoj <wojtek.fraczak@ericsson.com>
2023-05-22 20:50:08 -0400

    algo

--- code-resolution.md
+++ code-resolution.md
@@ -15,24 +15,26 @@ From other end:
 `<...>` - any union
 `<{ pattern fiels, ...}>`
 
-Code pattern will be a tree (and-or rooted graph) with edges labeled by `fields` and the following nodes:
+Code pattern will be a tree (and-or rooted graph) with edges labeled by `fields` and nodes
+labeled by open/close (*, .) and by type: unknown/union/product (<>, {}, //):
 
-1. `*`
+1. `/*/`
 2. `{*}`
 3. `<*>`
-4. `{}`
-5. `<>`
+4. `//`
+5. `{}`
+6. `<>`
 
-The graph only with "closed" nodes is "code".
+A graph without "open" and "unknown" nodes is "code".
 
 Partial order over code patterns (should agree with sets of values):
 
 - min element: single node `<>`,
-- max element: single node `*`,
+- max element: single node `/*/`,
 - `<*>(l1:p1) > <*>(l1:q1,l2:p2)`, if `p1 > q1`
 - `{*}(l1:p1) > {*}(l1:q1,l2:p2)`, if `p1 > q1`
-- `*(l1:p1) > <*>(l1:q1)`, if `p1 > q1`
-- `*(l1:p1) > {*}(l1:q1)`, if `p1 > q1`
+- `/*/(l1:p1) > <*>(l1:q1)`, if `p1 > q1`
+- `/*/(l1:p1) > {*}(l1:q1)`, if `p1 > q1`
 
 ## Questions
 
@@ -42,3 +44,52 @@ Partial order over code patterns (should agree with sets of values):
 `{*}(l1:p1) \cap {*}(l2:p2) = {*}(l1:p1,l2:p2)`
 `{}(l1:p1) \cap {}(l2:p2) = <>()` unless `l1=l2` otherwise `{}(l1:p1\cap p2)`
 
+## Pettern Graph
+
+Example:
+
+    $nat = < {} _, nat 0, nat 1>;
+    rlz = $nat <.0._ {{{} _} 0}, .0 rlz, ()>;
+
+We get `codes`:
+
+    id    |       code
+    ------|----------------------
+    '{}'  | {}
+    'nat' | <nat 0, nat 1, {} _>
+
+and the (singleton) forest of expressons:
+
+    id    | ref-name | rel
+    ------|----------|--------------
+    0     | 'rlz'    | ($nat < (.0 ._ {{{} _} 0}), (.0 rlz), ()>)
+
+### Initialize pattern graph
+
+1. We annotate the expression forest by unique pattern ids, all initialized as `*`, e.g.:
+
+        id | name  | rel*
+        ---|-------|-----------------
+        0  | 'rlz' |    (    $nat    <    (    .0    ._    {    {    {}    _}     0}     )    ,     (     .0     rlz     )     ,     ()     >     )     
+           |       | %0 ( %1 $nat %2 < %3 ( %4 .0 %5 ._ %6 { %7 { %8 {} %9 _} %10 0} %11 ) %12, %13 ( %14 .0 %15 rlz %16 ) %17 , %18 () %19 > %20 ) %21
+
+2. The pattern graph (with 22 nodes) has no edges.
+
+### Pattern graph folding
+
+Given a pattern graph, `(V,E)`, and an expression forest, `F`, we calculate a `folding` of the pattern graph,
+which consists of:
+
+1. equivalence classes `eq` over `V + C`
+2. new edges, `E'`
+3. pattern refinements, `R` 
+
+In our example:
+
+`eq: {0,1,nat,2,3,4,11,12,13,14,15,16,17,18,19,20,21}, {5}, {6,7,8}, {9}, {10}`
+`E': (4 -[0]-> 5), (5 -[_]-> 6), (10 -[_]-> 9), (11 -[0]-> 10), (14 -[0]-> 15)`
+`R`: 9:{}, 10:<*>
+
+By folding the pattern graph with those information we get:
+
+    %n ( %n $nat %n < %n ( %n .0 %n ._ %{} { %{} { %{} {} %{} _} %n 0} %n ) %n, %n ( %n .0 %n rlz %n ) %n , %n () %n > %n ) %n
---
+++ patterns.mjs
@@ -0,0 +1,145 @@
+const emptyPat = new Pattern("union",true); 
+const starPat = new Pattern(); 
+function isEmptyPat(pat) {
+  const {kind, closed, fields} = pat;
+  return kind === 'union' && closed && fields.size === 0;
+}
+
+function areMapsEqual(m1, m2) {
+  return m1.size === m2.size && Array.from(m1.keys()).every((key) => 
+    m1.get(key) === m2.get(key));
+}
+
+function cap(pat1, pat2, eq) {
+  if (pat1 === pat2) return pat1;
+  let kind, fields;
+  if (pat1.kind === null) {
+    kind = pat2.kind;
+  } else if (pat2.kind === null) {
+    kind = pat1.kind;
+  } else if (pat1.kind === pat2.kind) {
+    kind = pat1.kind;
+  } else 
+    return emptyPat;
+  
+  const fieldNames = new Set([...pat1.fields.keys(), ...pat2.fields.keys()]);
+
+  if ((pat1.closed && pat1.fields.size !== fieldNames.size) || 
+      (pat2.closed && pat2.fields.size !== fieldNames.size))
+    return emptyPat;
+  
+  fields = [...fieldNames].map(f => {
+    if (!pat1.fields.has(f)) 
+      return [f, findRep(eq, pat2.fields.get(f))];
+    if (!pat2.fields.has(f))
+      return [f, findRep(eq, pat1.fields.get(f))];
+    return [f, union(eq, pat1.fields.get(f), pat2.fields.get(f))];
+  });
+  return new Pattern(kind, closed1 || closed2, fields);
+}  
+
+class Pattern {
+  constructor(type=null, closed=null, fields=null) {
+    this.type = type; // null, "union", "product"
+    this.closed = closed; // true, false (=null)
+    this.fields = new Map(fields); // Map<field, PatternIdx>
+  }
+  isEmpty() {
+    return isEmptyPat(this);
+  }
+  equals(other) {
+    return this.type === other.type && this.closed === other.closed && 
+      areMapsEqual(this.fields, other.fields);
+  }
+  cap(other, eq) {
+    return cap(this, other, eq);
+  }
+
+}
+
+function annotate(rel, patterns) {
+  const i = patterns.length;
+  patterns.push(new Pattern()); 
+  const o = patterns.length;
+  patterns.push(new Pattern());
+  rel.patterns = [i, o];
+  switch (rel.op) {
+    case "product":
+      rel["product"].forEach(({label, exp}) => annotate(exp, patterns));
+      break;
+    case "union":
+    case "vector":
+      rel[rel.op].forEach((exp) => annotate(exp, patterns));
+  }
+}
+
+function union(eq,i,j) {
+  const ri = findRep(eq,i);
+  const rj = findRep(eq,j);
+  if (ri !== rj) {
+    if (Number.isInteger(ri)) {
+      eq[ri] = rj;
+      return rj;
+    } else if (Number.isInteger(rj)) {
+      eq[rj] = ri;
+      return ri;
+    } else
+      throw new Error("Cannot have two different codes in the same equivalence class");
+  }
+  return rj;
+}
+
+function findRep(eq, i) {
+  if (!Number.isInteger(i)) return i;
+  let j = i;
+  const path = [];
+  while (eq[j] !== j) {
+    path.push(j);
+    j = eq[j];
+  }
+  path.forEach((k) => (eq[k] = j));
+  return j;
+}
+
+function inspect(rel, patterns, eq) {
+  let changed = false;
+  switch (rel.op) {
+    case "code":
+      if ((findRep(eq,rel.patterns[0]) != rel.code) || 
+          (findRep(eq,rel.patterns[1]) != rel.code)) {
+        changed = true;
+        union(eq, eq,rel.patterns[0], union(eq, rel.patterns[1], rel.code));
+        return true;
+      }
+    case "product":
+      // TODO: 
+      break;
+    case "union":
+    case "vector":
+     
+  }
+}
+
+function patterns(codes, representatives, rels) {
+  // will return annotated rels; each rel will have a pair of "patterns" property (inCode, outCode)
+  // 1. initialize patterns (vector of patterns))
+  const patterns = [];
+  for (const relName in rels) {
+    const defs = rels[relName];
+    for (const def of defs) { annotate(def, patterns); }
+  }
+  const eq = patterns.map((_, i) => i); // equivalence classes (int = pattern index, str = code name)
+
+  // 2. loop until no more changes
+  let changed = true;
+  while (changed) { 
+    changed = false;
+    for (const relName in rels) {
+      const defs = rels[relName];
+      for (const def of defs) {
+        changed = inspect(def, patterns,eq) || changed;
+      }
+    }
+  }
+  
+}
