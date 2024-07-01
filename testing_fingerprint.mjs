2a07280c30f3872870cbca6848e89e3a69d21e34
wojtek <fraczak@gmail.com>
2024-07-01 02:07:26 -0400

    wip

--- testing_fingerprint.mjs
+++ testing_fingerprint.mjs
@@ -1,5 +1,28 @@
 import k from "./index.mjs";
 
+import blake from 'blakejs';
+import crypto from 'crypto';
+
+function logCeiling(n, base = 2) {
+  // Calculate the logarithm of n with the specified base
+  const logValue = Math.log(n) / Math.log(base);
+  
+  // Apply the ceiling function to the logarithm value
+  return Math.ceil(logValue);
+}
+
+
+
+// Function to generate a key from a secret string using PBKDF2
+function deriveKey(secret, salt='salt', keyLength=10) {
+  return crypto.pbkdf2Sync(secret, salt, 100000, keyLength, 'sha256');
+}
+function generateFingerprint(input,key = null) {
+  const hash = blake.blake2b(input, key, logCeiling(input.length,8)); 
+  const fingerprint = Buffer.from(hash).toString('hex');
+  return 'C'+fingerprint.toUpperCase();
+}
+
 function codeToString(code, codes) {
   var i = 0;
   const Q = [[code,i]];
@@ -10,7 +33,7 @@ function codeToString(code, codes) {
     const c_code = codes[x];
     if (c_code === undefined) {
       // result += `$${c}=${JSON.stringify(x)};`;
-      result += `${JSON.stringify(x)};`;
+      result += `${JSON.stringify(x)}`;
       continue;
     }
     const u_args =  Object.keys(c_code[c_code.code]).sort().map((k) => {
@@ -23,10 +46,10 @@ function codeToString(code, codes) {
     });
     if (c_code.code == "union") {
       //result += `$${c}=<${u_args.join(",")}>;`;
-      result += `<${u_args.join(",")}>;`;
+      result += `<${u_args.join(",")}>`;
     } else if (c_code.code == "product") {
       //result += `$${c}={${u_args.join(",")}};`;
-      result += `{${u_args.join(",")}};`;
+      result += `{${u_args.join(",")}}`;
     } else {
       throw new Error(`Unexpected code ${c_code.code}`);
     }
@@ -55,8 +78,8 @@ function codeToString(code, codes) {
   console.log(`// CODES: ${JSON.stringify(annotated.codes,"",2)}`);
   for (const code in annotated.codes) {
     console.log(`// CODE: ${code}`);
-    console.log(codeToString(code,annotated.codes));
+    const s = codeToString(code,annotated.codes);
+    console.log(generateFingerprint(s,deriveKey('kupa')));
+    console.log(generateFingerprint(s), s);
   }
-  console.log("// CODE: string");
-  console.log(codeToString("string",annotated.codes));
 });
